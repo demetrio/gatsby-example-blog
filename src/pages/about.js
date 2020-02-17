4f27a56e9fbc0674a45e02c350100415058e1c8f
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2020-02-17 21:13:13 +0100

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+    <>
+        <h1>
+            About Me
+        </h1>
+        <p>This is my personal website</p>
+        <Link to="/">Go to Home</Link> 
+    </>
+);
+
+
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+    <>
+        <h1>Hello!</h1>
+        <p>I hope you had a nice day</p>
+        <Link to="/about">Go to About!</Link> 
+    </>
+);
