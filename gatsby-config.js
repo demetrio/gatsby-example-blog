6a8afdc52720b9555f4017b368a9edad9e7064d1
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2020-02-18 23:23:34 +0100

    Add mdx contact me component

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     title: 'Gatsby Blog',
     description: 'This is a simple blog made with Gatsby',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+  ],
 };
