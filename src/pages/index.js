0fc2a21868c6856b6cc88090aa9ad3e8ca0b672c
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2020-02-18 01:03:46 +0100

    Adds basic structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from './components/layout';
 
 export default () => (
-    <>
-        <h1>Hello!</h1>
-        <p>I hope you had a nice day</p>
-        <Link to="/about">Go to About!</Link> 
-    </>
+  <Layout>
+    <h1>Hello!</h1>
+    <p>I hope you had a nice day</p>
+    <Link to="/about/">Learn about me &rarr;</Link>
+  </Layout>
 );
