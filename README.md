33a8d893c80cd5068d43f9d945281dd36af9c953
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2020-02-21 17:19:56 +0100

    Adds Intagram Source Pluggin

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 
 ## Installation instructions
 
-1. `npm install -g gatsby-cli`
+1. Have the Gatsby CLI (gatsby-cli) installed globally by running: `npm install -g gatsby-cli`
 1. `npm install`
 1. `npm run develop`
 1. Open http://localhost:8000/
@@ -17,3 +17,7 @@
 ### Suggestions
 
 1. Use `gatsby clean` if you add new folders
+
+### About this project
+
+If you want to know more about all the things [package.json](package.json) file.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
+import Layout from '../components/layout';
 import PostPreview from '../components/post-preview';
 import ReadLink from '../components/read-link';
 import Hero from '../components/hero';
+import Insta from './../components/insta';
 
 export default () => {
   const posts = usePosts();
@@ -14,11 +15,11 @@ export default () => {
         <h1>Home</h1>
         <p>You know what they say...it's hard to keep things simple.</p>
         <ReadLink to="/about/">Learn about me &rarr;</ReadLink>
-
         <h2>Read my blog</h2>
         {posts.map(post => (
           <PostPreview key={post.slug} post={post} />
         ))}
+        <Insta />
       </Layout>
     </>
   );
